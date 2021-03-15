a6b9956050ebe6048faa730d98fdbf0146cfb511
SinnottTM <tim.sinnott86@gmail.com>
2021-03-15 05:57:24 +0000

    radical redesign, changed how controller interacted with data

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -4,71 +4,86 @@ from flask import render_template, request, redirect
 from app.models.player import Player
 from app.models.game import Game
 
-# Logic for potential high score list
-from app.models.players_list_high_score import players, add_new_player, remove_player
+# Import for potential high scores list, unused
+# from app.models.players_list_high_score import players, add_new_player, remove_player
 
 # default route for GET, event page
-@app.route('/rpsmain')
+@app.route('/')
 def index():
     return render_template('gamestart.html', title='Rock Paper Scissors')
 
-# game logic, get result using different HTML pages
-app.route('/rpsmain/<choice_1>/<choice_2>')
-def run_game(choice_1, choice_2):
-    player_1 = Player("Player 1", choice_1)
-    player_2 = Player("Player 2", choice_2)
-    game = Game(player_1, player_2)
-    result = game.play_game(choice_1, choice_2)
+# game logic, player 1 versus player 2
+@app.route('/<choice_1>/<choice_2>')
+def game(choice_1, choice_2):
+    contest = Game(choice_1, choice_2)
+    victor = contest.play_game(choice_1, choice_2)
     
-    # Draw condition
-    if result == "D":
-        return render_template("gameenddraw.html",title="The Results")
+    return render_template(
+        "gameend.html", title="Rock Paper Scissors",
+        victor=victor, player_1_choice=choice_1, player_2_choice=choice_2,
+    )
 
-        # Player 1 via rock
-    elif result == "P1R":
-        return render_template("gameendp1rock.html", title="The Results")
+#################################################################################
+
+# Game logic, get result using different HTML pages. Abandoned as it didn't work
+# Also added a lot of extra HTML pages but would have been useful for unique splash pages etc.
+
+# app.route('/<choice_1>/<choice_2>')
+# def run_game(choice_1, choice_2):
+#     player_1 = Player("Player 1", choice_1)
+#     player_2 = Player("Player 2", choice_2)
+#     game = Game(player_1, player_2)
+#     result = game.play_game(choice_1, choice_2)
+    
+#     # Draw condition
+#     if result == "D":
+#         return render_template("gameenddraw.html",title="The Results")
+
+#         # Player 1 via rock
+#     elif result == "P1R":
+#         return render_template("gameendp1rock.html", title="The Results")
 
 
-        # Player 2 via paper
-    elif result == "P2P":
-        return render_template("gameendp2paper.html",title="The Results")
+#         # Player 2 via paper
+#     elif result == "P2P":
+#         return render_template("gameendp2paper.html",title="The Results")
 
 
-        # Player 2 via rock
-    elif result == "P2R":
-        return render_template("gameendp2rock.html",title="The Results")
+#         # Player 2 via rock
+#     elif result == "P2R":
+#         return render_template("gameendp2rock.html",title="The Results")
 
 
-        # Player 1 via scissors
-    elif result == "P1S":
-        return render_template("gameendp1scissors.html",title="The Results")
+#         # Player 1 via scissors
+#     elif result == "P1S":
+#         return render_template("gameendp1scissors.html",title="The Results")
 
-        # Player 2 via scissors
-    elif result == "P2S":
-        return render_template("gameendp2scissors.html",title="The Results")
+#         # Player 2 via scissors
+#     elif result == "P2S":
+#         return render_template("gameendp2scissors.html",title="The Results")
 
-        # Player 1 via paper
-    elif result == "P1P":
-        return render_template("gameendp1paper.html",title="The Results")
+#         # Player 1 via paper
+#     elif result == "P1P":
+#         return render_template("gameendp1paper.html",title="The Results")
 
 #################################################################################
 # Logic to add/remove player choices (maybe for previous scores etc?) Might need a new HTML page
 # # logic to add new event, POST
-# @app.route('/rps/add', methods=['POST'])
+# @app.route('/add', methods=['POST'])
 # def create():
 #     player_name = request.form['name']
 #     player_choice = request.form['choice']
 #     new_player = Player(player=player_name, choice=player_choice)
 #     add_new_player(new_player)
-#     return redirect('/rpsmain')
+#     return redirect('/')
 
 # # default route for GET, remove high score
-# @app.route('/rps/remove')
+# @app.route('/remove')
 # def index_2():
 #     return render_template('gamestart.html', title='Rock Paper Scissors', players=players)
 
 # # Logic to remove event, POST
-# @app.route('/rps/remove', methods=['POST'])
+# @app.route('/remove', methods=['POST'])
 # def remove():
 #     player_to_delete = None
 #     player_name = request.form['name']
@@ -77,5 +92,5 @@ def run_game(choice_1, choice_2):
 #             player_to_delete = player
 #             break
 #         remove_player(player_to_delete)
-#     return redirect('/rpsmain')
+#     return redirect('/')
 #################################################################################
--- app/models/game.py
+++ app/models/game.py
@@ -6,17 +6,17 @@ class Game():
     def play_game(self, player_1, player_2):
         player_1_choice = player_1
         player_2_choice = player_2
-        if player_1_choice == player_2_choice:
-            return "D"
-        elif player_1_choice == "rock" and player_2_choice == "scissors":
-            return "P1R"
-        elif player_1_choice == "rock" and player_2_choice == "paper":
-            return "P2P"
-        elif player_1_choice == "scissors" and player_2_choice == "rock":
-            return "P2R"
-        elif player_1_choice == "scissors" and player_2_choice == "paper":
-            return "P1S"
-        elif player_1_choice == "paper" and player_2_choice == "scissors":
-            return "P2S"
-        elif player_1_choice == "paper" and player_2_choice == "rock":
-            return "P1P"
+        if player_1_choice.lower() == player_2_choice.lower():
+            return "Draw"
+        elif player_1_choice.lower() == "rock" and player_2_choice.lower() == "scissors":
+            return "Player 2"
+        elif player_1_choice.lower() == "rock" and player_2_choice.lower() == "paper":
+            return "Player 2"
+        elif player_1_choice.lower() == "scissors" and player_2_choice.lower() == "rock":
+            return "Player 2"
+        elif player_1_choice.lower() == "scissors" and player_2_choice.lower() == "paper":
+            return "Player 1"
+        elif player_1_choice.lower() == "paper" and player_2_choice.lower() == "scissors":
+            return "Player 1"
+        elif player_1_choice.lower() == "paper" and player_2_choice.lower() == "rock":
+            return "Player 1"
